a033e36aea4ab030b199d8613e0e39398aef4b1d
MrAuthentik <umokeuchenna2020@gmail.com>
2022-10-19 19:35:28 -0700

    Nice

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,39 +1,20 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- *
- *   * Description: is written by honeydropjahbless
- *
- *    */
-
-
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * Description: Nesting In c
+ */
 
 void print_alphabet_x10(void)
-
 {
+	int count = 0;
+	char letter;
 
-		int count = 0;
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 
-			char letter;
-
-
-
-				while (count++ <= 9)
-
-						{
-
-									for (letter = 'a'; letter <= 'z'; letter++)
-
-													_putchar(letter);
-
-
-
-											_putchar('\n');
-
-												}
-
+		_putchar('\n');
+	}
 }
